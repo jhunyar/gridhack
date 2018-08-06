1e9c65180557166330f2a5cca0b852190b0ebec0
James Hunyar <jrhunyar@gmail.com>
2018-08-05 21:50:47 -0400

    Cleaning up todo list

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 // TODO:
-    // Create a buildRoom function that creates all 49 tiles (divs)
-    // Create a setActive function that intially sets the middle tile as active
+    // Build tiles as objects that have all of the properties from the array 
     // Create a findItem function that, upon RNG item drop, adds it to the current tile's array/object
     // Create an inventory UI
     // Create inventory object/array
     // Create a listener for 'g' that removes item from tile if present, and adds to inventory array
         // Create a refreshInventory function that redraws the inventory(?)
-
 // 
+
 buildRoom()
 
 // lets go ahead and reset the room to start fresh
