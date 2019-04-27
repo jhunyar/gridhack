aa9c38b17edded6f179d5b123a9d9dd3cb5248a0
James Hunyar <jrhunyar@gmail.com>
2019-04-26 23:18:25 -0400

    Use array map to build column arrays

--- public/scripts/gridhack-functions.js
+++ public/scripts/gridhack-functions.js
@@ -1,12 +1,12 @@
 // Misc variables
 let tileArray
 const visibleArray = [13, 14, 15, 1, -1, -13, -14, -15]
-const westWall = [-1, 13, 27, 41, 55, 69, 83, 97, 111, 125, 139, 153, 167, 181]
-const eastCol = [13, 27, 41, 55, 69, 83, 97, 111, 125, 139, 153, 167, 181, 195]
-const westCol = [0, 14, 28, 42, 56, 70, 84, 98, 112, 126, 140, 154, 168, 182]
 const eastWall = [14, 28, 42, 56, 70, 84, 98, 112, 126, 140, 154, 168, 182, 196]
+const westWall = [-1, 13, 27, 41, 55, 69, 83, 97, 111, 125, 139, 153, 167, 181]
 const northWall = [-15, -14, -13, -12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1]
 const southWall = [196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210]
+const eastCol = eastWall.map(n => n - 1)
+const westCol = westWall.map(n => n + 1)
 const tileInfoName = document.querySelector('#room-name')
 const tileInfoDesc = document.querySelector('#room-desc')
 const alert = document.querySelector('#alert')
