8678c0282a0bb1f1c2af433fabb75eae12a3da24
James Hunyar <james@homegauge.com>
2019-10-23 00:27:25 -0400

    Mobs will not collide with player

--- public/scripts/mobs.js
+++ public/scripts/mobs.js
@@ -28,10 +28,8 @@ const moveMobs = () => {
   mobTiles.forEach((tile) => {
     let move = Math.random()*4
 
-    if (move >= 0 && move < 1) {
-      if (!tiles[tile.mob.currentTile-1] 
-        || westWall.includes(tile.mob.currentTile-1)
-        || tiles[tile.mob.currentTile-1].mob != null) {
+    if (move >= 0 && move < 1) { // west
+      if (isBlocked(tile.id, -1)) {
         return
       } else {
         tile.mob.currentTile = tile.mob.currentTile-1
@@ -39,10 +37,8 @@ const moveMobs = () => {
         tiles[tile.mob.currentTile+1].mob = null
         resetTileEl(tile, -1)
       }
-    } else if (move >= 1 && move < 2) {
-      if (!tiles[tile.mob.currentTile+1]
-        || eastWall.includes(tile.mob.currentTile+1)
-        || tiles[tile.mob.currentTile+1].mob != null) {
+    } else if (move >= 1 && move < 2) { // east
+      if (isBlocked(tile.id, 1)) {
         return
       } else {
         tile.mob.currentTile = tile.mob.currentTile+1
@@ -50,10 +46,8 @@ const moveMobs = () => {
         tiles[tile.mob.currentTile-1].mob = null
         resetTileEl(tile, +1)
       }
-    } else if (move >= 2 && move < 3) {
-      if (!tiles[tile.mob.currentTile-14]
-        || northWall.includes(tile.mob.currentTile-14)
-        || tiles[tile.mob.currentTile-14].mob != null) {
+    } else if (move >= 2 && move < 3) { // north
+      if (isBlocked(tile.id, -14)) {
         return
       } else {
         tile.mob.currentTile = tile.mob.currentTile-14
@@ -61,10 +55,8 @@ const moveMobs = () => {
         tiles[tile.mob.currentTile+14].mob = null
         resetTileEl(tile, -14)
       }
-    } else if (move >= 3 && move < 4) {
-      if (!tiles[tile.mob.currentTile+14]
-        || southWall.includes(tile.mob.currentTile+14)
-        || tiles[tile.mob.currentTile+14].mob != null) {
+    } else if (move >= 3 && move < 4) { // south
+      if (isBlocked(tile.id, 14)) {
         return
       } else {
         tile.mob.currentTile = tile.mob.currentTile+14
@@ -74,6 +66,20 @@ const moveMobs = () => {
       }
     }
   })
+}
+
+const isBlocked =(tileId, offset)=> {
+  let wall
+  let tiles = dungeon.floors[player.currentFloor].tiles
+
+  if (offset === -1) { wall = westWall }
+    else if (offset === 1) { wall = eastWall }
+    else if (offset === -14) { wall = northWall }
+    else if (offset === 14) { wall = southWall }
+
+  if (!tiles[tileId] || wall.includes(tileId+offset) || tiles[tileId+offset].mob != null || player.currentTile === tileId+offset) {
+    return true
+  }
 }
 
 // Reset a specifc tile after a mob movement
